ebe3731fbb43a3ccdcc2d600fcf834ef64cedbef
Eden <edenstevens101@gmail.com>
2017-03-30 16:49:11 +0100

    Added save method to Artist Class

--- models/artist.rb
+++ models/artist.rb
@@ -10,3 +10,9 @@ class Artist
     @id = options['id'].to_i
   end
 end
+
+def save()
+  sql = "INSERT INTO artists (name, bio) VALUES ('#{@name}', '#{@bio}') RETURNING *;"
+  artist_data = SqlRunner.run(sql)
+  @id = artist_data.first()['id']
+end
