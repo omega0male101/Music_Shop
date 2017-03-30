127cb83c61fac819fe35c9c8ada1ee648956c385
Eden <edenstevens101@gmail.com>
2017-03-30 15:50:32 +0100

    Created DB with tables

--- db/music_shop.sql
+++ db/music_shop.sql
@@ -2,15 +2,6 @@ DROP TABLE albums;
 DROP TABLE artists;
 DROP TABLE genres;
 
-CREATE TABLE albums (
-  id SERIAL8 PRIMARY KEY,
-  title VARCHAR(255),
-  artist_id INT4 REFERENCES artist(id),
-  genre_id INT4 REFERENCES genre(id),
-  artwork TEXT,
-  quantity INT2
-  );
-
 CREATE TABLE artists (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
@@ -20,4 +11,13 @@ CREATE TABLE artists (
 CREATE TABLE genres (
   id SERIAL8 PRIMARY KEY,
   type VARCHAR(255)
+  );
+
+CREATE TABLE albums (
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  artist_id INT4 REFERENCES artists(id),
+  genre_id INT4 REFERENCES genres(id),
+  artwork TEXT,
+  quantity INT2
   );
