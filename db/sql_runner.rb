a63e8b2a03726030a0eba28775ed82b30266f5fc
Eden <edenstevens101@gmail.com>
2017-05-10 15:40:25 +0100

    added creds

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,10 @@ class SqlRunner
 
   def self.run( sql )
     begin
-      db = PG.connect({ dbname: 'music_shop', host: 'localhost' })
+      # db = PG.connect({ dbname: 'music_shop', host: 'localhost' })
+      db = PG.connect({dbname: 'd7sir27q68q412', 
+host: 'ec2-54-163-234-20.compute-1.amazonaws.com', 
+port: 5432, user: 'edenstevens101+heroku@gmail.com', password: 'Qwerty123'})
       result = db.exec( sql )
     ensure
       db.close
